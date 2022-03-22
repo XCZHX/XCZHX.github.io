a156281921ebef9169eee5fbf6711a11f0119ed9
XCZHX <1207427003@qq.com>
2022-03-22 10:58:23 +0800

    Site updated: 2022-03-22 10:58:23

---
+++ dist/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [
+        {
+            name: '飞鸟和蝉',
+            artist: '阿梓',
+            url: 'http://r93effwkn.bkt.clouddn.com/music/%E9%A3%9E%E9%B8%9F%E5%92%8C%E8%9D%89.mp3'
+            //cover: 'cover.jpg'
+        }
+    ]
+})
